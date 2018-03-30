b4f2960d4b0f3568241dfa19577923b7248756d6
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2018-03-30 17:07:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Alt Three UUID
+
+A UUID to 64-bit signed integer converter.
+
+This is a lossy conversion, performed by xoring the both halves.
+
+
+## Installation
+
+This version requires 64-bit [PHP](https://php.net) 7.1 or 7.2.
+
+To get the latest version, simply require the project using [Composer](https://getcomposer.org):
+
+```bash
+$ composer require alt-three/uuid
+```
+
+
+## Security
+
+If you discover a security vulnerability within this package, please e-mail us at support@alt-three.com. All security vulnerabilities will be promptly addressed.
+
+
+## License
+
+Alt Three Storage is licensed under [The MIT License (MIT)](LICENSE).
---
+++ src/UuidConverter.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Alt Three UUID.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AltThree\Uuid;
+
+/**
+ * This is the uuid converter class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+final class UuidConverter
+{
+    /**
+     * Convert a uuid to a 64-bit signed integer.
+     *
+     * This is a lossy conversion, performed by xoring the both halves.
+     *
+     * @param string $uuid
+     *
+     * @return int
+     */
+    public static function convert(string $uuid)
+    {
+        // convert to 16 byte binary represention
+        $bin = hex2bin(str_replace(['{', '-', '}'], '', $uuid));
+
+        // xor first half with second to give 8 bytes
+        $xor = self::xor(...str_split($bin, 8));
+
+        // convert to a 64-bit signed integer
+        return unpack('q', $xor)[1];
+    }
+
+    /**
+     * Bitwise xor both strings.
+     *
+     * @param string $a
+     * @param string $b
+     *
+     * @return string
+     */
+    private static function xor(string $a, string $b)
+    {
+        return $a ^ $b;
+    }
+}
